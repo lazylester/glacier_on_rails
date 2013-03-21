bc54798c1aad3bd5eceb1227080f04d383ee699a
Les Nightingill <codehacker@comcast.net>
2013-03-20 17:54:20 -0700

    first attempt at Rails3 style mailer for server testing

--- app/models/backup_mailer.rb
+++ app/models/backup_mailer.rb
@@ -1,9 +1,13 @@
 class BackupMailer < ActionMailer::Base
   def backup_file(zipfile)
-    recipients  BACKUP_RECIPIENTS
-    from        "#{ORGANIZATION_NAME} Database Administrator<#{ADMIN_EMAIL}>"
-    headers     "Reply-to" => "#{ADMIN_EMAIL}", "Message-ID"=>"<#{DateTime.now.to_formatted_s(:number)}@#{SITE_URL}>"
-    subject     "#{ORGANIZATION_NAME} daily backup file"
-    attachment :content_type => "application/x-gzip", :body => zipfile, :filename => DateTime.now.to_formatted_s(:db)+"_production_backup.zip"
+    mail(:to => BACKUP_RECIPIENTS,
+         :from => "#{ORGANIZATION_NAME} Database Administrator<#{ADMIN_EMAIL}>",
+         :headers => ["Reply-to" => "#{ADMIN_EMAIL}",
+                      "Message-ID"=>"<#{DateTime.now.to_formatted_s(:number)}@#{SITE_URL}>"],
+         :subject => "#{ORGANIZATION_NAME} daily backup file",
+         :attachments => DateTime.now.to_formatted_s(:db)+"_production_backup.zip" =>
+                               {:mime_type => "application/x-gzip",
+                                :content => zipfile}
+         )
   end
 end
--- lib/tasks/email_backup.rake
+++ lib/tasks/email_backup.rake
@@ -2,5 +2,5 @@ desc "Send email with daily backup"
 task :email_backup => "db:backup" do
   puts "sending backup file by email"
   backfile = BackupFile.most_recent
-  BackupMailer.deliver_backup_file(backfile.gzipped)
+  BackupMailer.backup_file(backfile.gzipped).deliver
 end
