c4c3d7b199ff372412d2483e565f040e1220c71b
Les Nightingill <codehacker@comcast.net>
2013-03-20 17:15:34 -0700

    change backup_mailer parameters to be configured by application constants

--- app/models/backup_mailer.rb
+++ app/models/backup_mailer.rb
@@ -1,9 +1,9 @@
 class BackupMailer < ActionMailer::Base
   def backup_file(zipfile)
-    recipients  'Les Nightingill<les@stillpointoftheturningworld.com>'
-    from        "ORI Database Administrator<#{ADMIN_EMAIL}>"
-    headers     "Reply-to" => "#{ADMIN_EMAIL}", "Message-ID"=>"<#{DateTime.now.to_formatted_s(:number)}@orphansofrwanda.info>"
-    subject     "ORI daily backup file"
+    recipients  BACKUP_RECIPIENTS
+    from        "#{ORGANIZATION_NAME} Database Administrator<#{ADMIN_EMAIL}>"
+    headers     "Reply-to" => "#{ADMIN_EMAIL}", "Message-ID"=>"<#{DateTime.now.to_formatted_s(:number)}@#{SITE_URL}>"
+    subject     "#{ORGANIZATION_NAME} daily backup file"
     attachment :content_type => "application/x-gzip", :body => zipfile, :filename => DateTime.now.to_formatted_s(:db)+"_production_backup.zip"
   end
 end
--- lib/tasks/get_back_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :get_back do
-#   # Task goes here
-# end
