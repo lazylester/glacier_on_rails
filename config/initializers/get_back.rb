89b5a630670608a8036b050697c08d252161d28b
Les Nightingill <codehacker@comcast.net>
2017-05-07 09:25:35 -0700

    hook local migrations into app's migration list

--- config/initializers/get_back.rb
+++ config/initializers/get_back.rb
@@ -1,1 +1,6 @@
 BACKUP_DIR = Rails.env.production? ? Rails.root.join("../../shared/backups") : "#{Rails.root}/db/backups/"
+
+GetBack::Engine.config.paths["db/migrate"].expanded.each do |expanded_path|
+  Rails.application.config.paths["db/migrate"] << expanded_path
+end
+
