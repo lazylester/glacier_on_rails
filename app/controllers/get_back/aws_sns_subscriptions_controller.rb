92640d749559676ccf2a8f67ac19af7565698ba3
Les Nightingill <codehacker@comcast.net>
2017-05-07 05:32:19 -0700

    fixes errors in subscription notification action

--- app/controllers/get_back/aws_sns_subscriptions_controller.rb
+++ app/controllers/get_back/aws_sns_subscriptions_controller.rb
@@ -1,13 +1,13 @@
+require 'httparty'
+
 module GetBack
   class AwsSnsSubscriptionsController < ApplicationController
     skip_before_action :verify_authenticity_token, :check_permissions, :only=>[:create]
     def create
-      logger.info request.headers.env
-      logger.info request.raw_post
       if request.headers["x-amz-sns-message-type"] == "SubscriptionConfirmation"
         subscribe_url = JSON.parse(request.raw_post)["SubscribeURL"]
         raise MessageWasNotAuthentic unless subscribe_url =~ /^https.*amazonaws\.com\//
-        HTTParty.get sns.subscribe_url
+        HTTParty.get subscribe_url
         head :ok and return
       end
     end
