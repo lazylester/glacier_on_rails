6ed38d9c53d4cd475b56de12f56c764fec6f9c4d
Les Nightingill <codehacker@comcast.net>
2017-05-11 12:57:06 -0700

    further changes to postgres adapter to accommodate constraints of server

--- app/models/application_database/postgres_adapter.rb
+++ app/models/application_database/postgres_adapter.rb
@@ -7,38 +7,34 @@ class ApplicationDatabase::PostgresAdapter
 
   def zipped_contents
     system("touch tmp/temp_file.gz")
-    system("#{pg_dump} -w -Fc --dbname=#{db_connection_uri} > tmp/temp_file.gz")
+    system("#{pg_dump} -w -Fc #{db_config['database']} > tmp/temp_file.gz")
     File.read("tmp/temp_file.gz")
   end
 
   def zip_and_save_to_file(file)
     system("touch #{file}")
     # see http://stackoverflow.com/a/31599308/451893
-    system("#{pg_dump} -w -Fc --dbname=#{db_connection_uri} > #{file}")
+    system("#{pg_dump} -w -Fc #{db_config['database']} > #{file}")
     $?.exitstatus.zero?
   end
 
   def save_to_file(file)
     system("touch #{file}")
-    system("#{pg_dump} -w -Fp --clean --dbname=#{db_connection_uri} > #{file}")
+    system("#{pg_dump} -w -Fp --clean #{db_config['database']} > #{file}")
     $?.exitstatus.zero?
   end
 
   def restore_from_file(file)
-    system("#{psql} --dbname=#{db_connection_uri} < #{file}")
+    system("#{psql} #{db_config['database']} < #{file}")
     $?.exitstatus.zero?
   end
 
   def restore_from_zipfile(file)
-    system("#{pg_restore} --clean --dbname=#{db_connection_uri} #{file}")
+    system("#{pg_restore} --clean #{db_config['database']} #{file}")
     $?.exitstatus.zero?
   end
 
 private
-
-  def db_connection_uri
-    "postgresql://#{db_config['username']}:#{db_config['password']}@127.0.0.1:5432/#{db_config['database']}"
-  end
 
   def psql
     `which psql`.strip
