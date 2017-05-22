4479e3f499a0447436a2e913f2be7cf9396326ed
Les Nightingill <codehacker@comcast.net>
2017-05-22 08:21:26 -0700

    fixes initiation of archive retrieval job

--- app/controllers/get_back/aws_archive_retrieval_jobs_controller.rb
+++ app/controllers/get_back/aws_archive_retrieval_jobs_controller.rb
@@ -1,7 +1,7 @@
 class GetBack::AwsArchiveRetrievalJobsController < ApplicationController
   def create
     archive = GlacierArchive.find(params[:archive_id])
-    AwsBackend.new.retrieve_db_archive(archive)
+    archive.initiate_retrieve_job
     @glacier_archives = GlacierArchive.all
     render :partial => 'glacier_archive', :collection => @glacier_archives, :as => :archive
   end
