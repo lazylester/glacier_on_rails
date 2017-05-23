7e4dcad824d2151726f8b8ed351b81f5e25b6350
Les Nightingill <codehacker@comcast.net>
2017-05-22 18:28:43 -0700

    fixes bugs in aws_archives_controller

--- app/controllers/get_back/aws_archives_controller.rb
+++ app/controllers/get_back/aws_archives_controller.rb
@@ -2,12 +2,12 @@ class GetBack::AwsArchivesController < ApplicationController
   def fetch
     @archive = GlacierArchive.find(params[:archive_id])
     @archive.fetch_archive if @archive
-    render :partial => 'glacier_archive', :collection => GlacierArchive.all, :as => :archive
+    render :partial => 'get_back/aws_archive_retrieval_jobs/glacier_archive', :collection => GlacierArchive.all, :as => :archive
   end
 
   def restore
     @archive = GlacierArchive.find(params[:archive_id])
-    if @archive.restore
+    if @archive.restore_compressed
       render :js => "flash.set('confirm_message', 'database restored to #{@archive.created_at.to_date.to_s(:default)}');flash.notify();"
     else
       render :js => "flash.set('error_message', 'database restore failed');flash.notify();"
