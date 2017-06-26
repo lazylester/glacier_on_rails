6c3802891916914f2a0f255d541162aa70ef2482
Les Nightingill <codehacker@comcast.net>
2017-06-26 11:01:11 -0700

    add database configuration error checking with diagnostic message

--- app/models/application_database.rb
+++ app/models/application_database.rb
@@ -1,14 +1,21 @@
 class ApplicationDatabase
   attr_accessor :adapter
+  RequiredKeys = %w{username host database encoding adapter}
+  DbConfig = ActiveRecord::Base.configurations[Rails.env]
 
   def initialize
-    db_config = ActiveRecord::Base.configurations[Rails.env]
     @adapter = case db_config["adapter"]
                when "postgresql"
                  PostgresAdapter.new(db_config)
                when "mysql"
                  MysqlAdapter.new(db_config)
                end
+  end
+
+  def db_config
+    missing_keys = RequiredKeys - DbConfig.keys
+    raise "#{missing_keys.join(' and ')} must be specified in config/database.yml" unless missing_keys.empty?
+    DbConfig
   end
 
   delegate :contents, :restore, :to => :adapter
